fc49238472df4998c1bf870f817b888f2ec2e7db
chad ruppert <chad.ruppert@gmail.com>
2014-01-03 18:27:29 -0500

    initial commit

---
+++ IPTesting/IPTesting/Tests/RangeTests.js
@@ -0,0 +1,115 @@
+﻿///<reference path="/lib/Base.min.js"/>
+///<reference path="/lib/iplib.js"/>
+///<reference path="/lib/jasmine.js"/>
+///<reference path="/ipRangeToCidrList.js"/>
+describe("FixedLengthCollection push Tests", function() {
+    var converter = new ipRangeToCidrList();
+
+    beforeEach(function () { });
+    
+    it("should convert an Ip to a Number and back again", function () {
+
+        var ip = "192.168.1.1";
+
+        var num = converter.ipToNumber(ip);
+
+        var ipRevert = converter.numberToIp(num);
+        expect(ipRevert).toBe(ip);
+    });
+    it("should convert an Ip range to a list of cidr ranges", function () {
+
+        var ipStart = "192.168.1.25";
+        var ipEnd = "192.168.1.55";
+
+        var cidrs = converter.convert(ipStart, ipEnd);
+
+        
+        expect(cidrs.length).toBe(5);
+    });
+    it("should convert a 1 increment Ip range to a single cidr ranges", function () {
+
+        var ipStart = "192.168.1.32";
+        var ipEnd = "192.168.1.33";
+
+        var cidrs = converter.convert(ipStart, ipEnd);
+
+
+        expect(cidrs.length).toBe(1);
+    });
+    it("should convert a 3 increment Ip range to a single cidr ranges", function () {
+
+        var ipStart = "192.168.1.32";
+        var ipEnd = "192.168.1.35";
+
+        var cidrs = converter.convert(ipStart, ipEnd);
+
+
+        expect(cidrs.length).toBe(1);
+    });
+
+    it("should convert a large single subnet Ip range to a set of cidr ranges", function () {
+
+        var ipStart = "192.168.1.88";
+        var ipEnd = "192.168.1.254";
+
+        var cidrs = converter.convert(ipStart, ipEnd);
+        var ranges = [
+            "192.168.1.88/29", "192.168.1.96/27", "192.168.1.128/26", "192.168.1.192/27", "192.168.1.224/28", "192.168.1.240/29", "192.168.1.248/30", "192.168.1.252/31", "192.168.1.254/32"
+        ];
+        for (var i = 0; i < ranges.length; i++) {
+            expect(cidrs[i]).toBe(ranges[i]);
+        }
+        expect(cidrs.length).toBe(9);
+    });
+    it("should convert a full range single subnet Ip range to a set of cidr ranges", function () {
+
+        var ipStart = "192.168.1.2";
+        var ipEnd = "192.168.1.254";
+
+        var cidrs = converter.convert(ipStart, ipEnd);
+        var ranges = [
+            "192.168.1.2/31"
+            ,"192.168.1.4/30"
+            ,"192.168.1.8/29"
+            ,"192.168.1.16/28"
+            ,"192.168.1.32/27"
+            ,"192.168.1.64/26"
+            ,"192.168.1.128/26"
+            ,"192.168.1.192/27"
+            ,"192.168.1.224/28"
+            ,"192.168.1.240/29"
+            ,"192.168.1.248/30"
+            ,"192.168.1.252/31"
+            ,"192.168.1.254/32"
+        ];
+        for (var i = 0; i < ranges.length; i++) {
+            expect(cidrs[i]).toBe(ranges[i]);
+        }
+        expect(cidrs.length).toBe(13);
+    });
+    it("should convert a wide range multiple subnet Ip range to a set of cidr ranges", function () {
+
+        var ipStart = "192.168.1.2";
+        var ipEnd = "192.168.3.155";
+
+        var cidrs = converter.convert(ipStart, ipEnd);
+        var ranges = [
+            "192.168.1.2/31"
+            ,"192.168.1.4/30"
+            ,"192.168.1.8/29"
+            ,"192.168.1.16/28"
+            ,"192.168.1.32/27"
+            ,"192.168.1.64/26"
+            ,"192.168.1.128/25"
+            ,"192.168.2.0/24"
+            ,"192.168.3.0/25"
+            ,"192.168.3.128/28"
+            ,"192.168.3.144/29"
+            ,"192.168.3.152/30"
+        ];
+        for (var i = 0; i < ranges.length; i++) {
+            expect(cidrs[i]).toBe(ranges[i]);
+        }
+        expect(cidrs.length).toBe(12);
+    });
+});
---
+++ IPTesting/IPTesting/ipRangeToCidrList.js
@@ -0,0 +1,67 @@
+﻿function ipRangeToCidrList() {
+
+    this.CidrMask = [0x00000000, 0x80000000,
+        0xC0000000, 0xE0000000, 0xF0000000, 0xF8000000, 0xFC000000,
+        0xFE000000, 0xFF000000, 0xFF800000, 0xFFC00000, 0xFFE00000,
+        0xFFF00000, 0xFFF80000, 0xFFFC0000, 0xFFFE0000, 0xFFFF0000,
+        0xFFFF8000, 0xFFFFC000, 0xFFFFE000, 0xFFFFF000, 0xFFFFF800,
+        0xFFFFFC00, 0xFFFFFE00, 0xFFFFFF00, 0xFFFFFF80, 0xFFFFFFC0,
+        0xFFFFFFE0, 0xFFFFFFF0, 0xFFFFFFF8, 0xFFFFFFFC, 0xFFFFFFFE,
+        0xFFFFFFFF];
+
+    this.ipToNumber = function (ip) {
+        var parts = ip.split(".");
+        if (parts.length != 4) {
+            throw ("ip address is malformed.");
+        }
+        var d = parts;
+        return ((((((+d[0]) * 256) + (+d[1])) * 256) + (+d[2])) * 256) + (+d[3]);
+    };
+    this.numberToIp = function (num) {
+        var t = "";
+        t += ((num >> 24) & 255).toString(); 
+        t += ".";
+        t += ((num >> 16) & 255).toString(); 
+        t += ".";
+        t += ((num >> 8) & 255).toString();
+        t += ".";
+        t += (num & 255).toString();
+        return t;
+    };
+
+    this.convert = function (ipStart, ipEnd) {
+
+        var start = this.ipToNumber(ipStart);
+        var end = this.ipToNumber(ipEnd);
+
+        var results = [];
+        while (end >= start) {
+            var maxSize = 32 & 0xff;
+            while (maxSize > 0) {
+                var mask = this.CidrMask[maxSize - 1];
+                var maskBase = (start & mask) >>> 0;
+                var calcMaxIp = start + Math.pow(2, (32 - maxSize));
+                if (calcMaxIp > end+1) {
+                    maxSize++;
+                    break;
+                }
+                if (maskBase != start && calcMaxIp <= end) {
+                    break;
+                }
+                maxSize--;
+            }
+            var x = (Math.log(end - start + 1) / Math.log(2)) & 0xff;
+            var maxDiff = (32 / Math.floor(x)) & 0xff;
+            if (x!= 1 && maxSize < maxDiff) {
+                maxSize = maxDiff;
+            }
+            var newIp = this.numberToIp(start);
+            results.push(newIp + "/" + maxSize);
+            start += Math.pow(2, (32 - maxSize));
+            var np = this.numberToIp(start);
+            console.log(np);
+        }
+        return results;
+    };
+};
+
